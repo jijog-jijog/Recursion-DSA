2a5d0e171b81ffd8aaec6788107f0b3678438747
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-24 22:03:17 +0530

    d

---
+++ src/countnumbers.java
@@ -0,0 +1,14 @@
+public class countnumbers {
+    public static void main(String[] args) {
+        String allowd = "ab";
+        String[] words = {"ad","bd","aaab","baa","badab"};
+        int count = 0;
+
+        for (String a : words){
+            if (allowd.contains(a)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
