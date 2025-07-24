421d284d08ceb676f0c6162230f2b5d4d98b710d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-24 21:08:09 +0530

    r

--- src/primecount.java
+++ src/primecount.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class primecount {
+    public static void main(String[] args) {
+        int left = 6;
+        int right = 10;
+        int pricount = 0;
+
+        for (int i = left; i<=right; i++){
+
+            int val = i;
+            int count = 0;
+            while(val > 0){
+                if ((val&1)==1){
+                    count++;
+                }
+                val = val>>1;
+            }
+//            System.out.println(count);
+            if (isboolean(count)){
+                pricount++;
+            }
+
+        }
+        System.out.println(pricount);
+    }
+    static boolean isboolean(int n){
+        if(n <= 1){
+            return false;
+        }
+        for (int i =2; i*i<=n; i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
