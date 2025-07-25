43d95a51d7f6c26086aec20e57fca81e7210a9ed
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-25 16:38:24 +0530

    h

--- src/binaryswap.java
+++ src/binaryswap.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class binaryswap {
+    public static void main(String[] args) {
+        int  n = 2147483644;
+        int result = 0;
+
+
+
+        for (int i = 0; i<32; i++) {
+            int ans = (n&1);
+            result = (result<<1) | (ans);
+            n = n>>1;
+
+        }
+        System.out.println(result);
+
+
+    }
 }
