6562630e139dd18fd1f56ebd0b7fb2be39bc8e3a
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-25 18:03:32 +0530

    j

--- src/primecount.java
+++ src/primecount.java
@@ -14,9 +14,10 @@ public class primecount {
             int count = 0;
 
             while(val > 0){
-                
+
                 if ((val&1)==1){
 
+                    
                     count++;
                 }
                 val = val>>1;
