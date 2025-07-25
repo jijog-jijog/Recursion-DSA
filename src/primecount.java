bb95008560009b0c32eb384ff5cb15646002624b
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-25 18:06:03 +0530

    g

--- src/primecount.java
+++ src/primecount.java
@@ -10,9 +10,10 @@ public class primecount {
                 if ((val&1)==1){
                 count++;
                 }
+                
                val = val>>1;
             }
-            
+
             if (isboolean(count)){
                 pricount++;
             }
