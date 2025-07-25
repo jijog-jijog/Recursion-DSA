dc131e3f54f420f28a306cd91de505f118abcaf7
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-25 18:05:51 +0530

    g g

--- src/primecount.java
+++ src/primecount.java
@@ -22,8 +22,9 @@ public class primecount {
         if(n <= 1){
             return false;
         }
-        
+
         for (int i =2; i*i<=n; i++){
+            
             if (n % i == 0){
                 return false;
             }
