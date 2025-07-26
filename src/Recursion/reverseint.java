321dae0549a907042dd408456dbed0f85ccc1584
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-26 19:34:44 +0530

    Fix reverse function logic in reverseint.java to correctly check for palindrome integers

--- src/Recursion/reverseint.java
+++ src/Recursion/reverseint.java
@@ -2,6 +2,18 @@ package Recursion;
 
 public class reverseint {
     public static void main(String[] args) {
+        int n = 12321;
+        System.out.println(reverse(n));
+    }
+    static boolean reverse(int n){
+        if (n == 0){
+            return true;
+        }
+//        System.out.print(n%10);
 
+        if (n == reverse(n)){
+            return true;
+        }
+        reverse(n/10);
     }
 }
