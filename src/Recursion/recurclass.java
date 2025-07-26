c7ac9f7445f102a888d062fb042cbad32562b298
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-26 14:35:38 +0530

    Implement recursive functions ftz and ztf in recurclass.java

--- src/Recursion/recurclass.java
+++ src/Recursion/recurclass.java
@@ -1,4 +1,24 @@
 package Recursion;
 
 public class recurclass {
+    public static void main(String[] args) {
+        int n = 5;
+//        ftz(n);
+        ztf(n);
+    }
+     static void ftz(int n){
+        if (n==0){
+            return;
+        }
+
+        System.out.println(n);
+        ftz(n-1);
+    }
+    static void ztf(int n){
+        if (n==0){
+            return;
+        }
+        ztf(n-1);
+        System.out.println(n);
+    }
 }
