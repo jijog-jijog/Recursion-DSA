282dec9a492929b850535f782c62c8f5b8c472b6
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-08-12 10:43:52 +0530

    dt

--- src/Array/bubSor.java
+++ src/Array/bubSor.java
@@ -1,4 +1,32 @@
 package Array;
 
+import java.util.Arrays;
+
 public class bubSor {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 0, 3};
+        bubble(arr, arr.length - 1, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr, int e, int s) {
+        // Base case: If end index is 0, array is sorted
+        if (e == 0) {
+            return;
+        }
+
+        // If we haven’t reached the end of this pass
+        if (s < e) {
+            if (arr[s] > arr[s + 1]) {
+                int temp = arr[s];
+                arr[s] = arr[s + 1];
+                arr[s + 1] = temp;
+            }
+            // Move to the next element in this pass
+            bubble(arr, e, s + 1);
+        } else {
+            // Start a new pass, reduce end boundary
+            bubble(arr, e - 1, 0);
+        }
+    }
 }
--- src/Array/patternTriangle.java
+++ src/Array/patternTriangle.java
@@ -2,7 +2,25 @@ package Array;
 
 public class patternTriangle {
     public static void main(String[] args) {
+        int r = 1;
+        int c = 0;
 
+        pattern(r, c);
 
+    }
+    static void pattern(int r, int c){
+
+        if (r == 0){
+            return;
+        }
+
+        if (r > c){
+            pattern(r+1, c);
+            System.out.print("* ");
+        }else{
+
+           pattern(r-1, 0);
+            System.out.println();
+        }
     }
 }
