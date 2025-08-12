471dc2b077577faf0ed35ea5bacd9e344b4cc3df
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-08-12 16:20:04 +0530

    dt

--- src/Array/Selsort.java
+++ src/Array/Selsort.java
@@ -4,25 +4,10 @@ import java.util.Arrays;
 
 public class Selsort {
     public static void main(String[] args) {
-        int[] arr = {1,5,3,2,7,8,6};
-        selection(arr,arr.length-1, 0, 0 );
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {1,3,5,6,2,0};
+
     }
-    static void selection(int[] arr, int end, int start, int max) {
-        if (end == 0) {
-            return;
-        }
-        if (start < end) {
-            if (arr[start] > arr[max]) {
-                selection(arr, end, start+1, start);
-            } else {
-                selection(arr, end, start+1, max);
-            }
-        } else {
-            int temp = arr[max];
-            arr[max] = arr[end-1];
-            arr[end-1] = temp;
-            selection(arr, end-1, 0, 0);
-        }
+    static void selection(int[] arr, int start, int end, int max){
+
     }
 }
